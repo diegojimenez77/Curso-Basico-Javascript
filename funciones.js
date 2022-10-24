dbaa191ffe021d169320562367353c4cf76e843a
diegojimenez77 <gjpcoding8652@gmail.com>
2022-10-24 14:51:19 -0500

    adding variables and Funciones files

---
+++ funciones.js
@@ -0,0 +1,13 @@
+//Declarativas
+function miFuncion (){
+    return 3;
+}
+
+muFuncion();
+
+//Expresion o funciones anonimas
+var miFuncion = function (a,b){
+    return a + b;
+}
+
+miFuncion();
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Oscar";
+
+// declarar
+var edad;
+edad
+// inicializar
+edad = 30;
+edad
+//array
+var elementos = ["Computadora", "Celular"];
+
+//objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
